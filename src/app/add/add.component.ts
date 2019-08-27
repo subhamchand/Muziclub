b9d11456b99995fd181265250d2c20a3b1f04d96
Your Name <DM00507444@techmahindra.com>
2019-08-27 18:13:29 +0530

    date restriction to 3 days

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -3,53 +3,56 @@ import { Component, OnInit } from '@angular/core';
 import { BookslotService } from '../bookslot.service';
 import * as  moment from 'moment';
 
+const regExp = /^[1-9]{1}[0-9]{9}$/;
+
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
+  mindate = new Date();
+  maxdate = moment(new Date()).add(3, 'days').format();
   startArraytimes = [];
   endArraytimes = [
     {
-    title: '30 MIN', value: '30'
-   },
-   {
-    title: '1 HOUR', value: '60'
-  }
-];
+      title: '30 MIN', value: '30'
+    },
+    {
+      title: '1 HOUR', value: '60'
+    }
+  ];
   isValidTime = false;
   isSelectedDate = false;
   timeArray = [];
-
+  endDuration;
 
   user = {
     name: '',
     contact: '',
     bookingdate: null,
     starttime: '',
-    endtime: ''
+    endtime: null,
+    comment: ''
   };
 
   constructor(private bookSlotService: BookslotService,
-              private dateService: DateService) {
+    private dateService: DateService) {
   }
 
   ngOnInit() {
     this.startArraytimes = this.bookSlotService.getStartTimesArray('monday')[0].times;
     this.timeArray = this.dateService.dateArray();
-    console.log(this.timeArray);
   }
   onSelectStartTime(event) {
     this.user.starttime = moment(this.user.bookingdate).format('l') + ' ' + event.value;
-    console.log(this.user.starttime);
+    console.log(moment(new Date()).format('l') + ' ' + event.value);
     this.formValidate();
 
   }
 
   onSelectEndTime(event) {
-    this.user.endtime = moment(this.user.bookingdate).format('l') + ' ' + event.value;
-    console.log(this.user.endtime);
+    this.endDuration = event.value;
     this.formValidate();
 
 
@@ -57,9 +60,7 @@ export class AddComponent implements OnInit {
 
   onDateSelect(event) {
     this.user.bookingdate = event.value;
-    console.log(this.user.bookingdate);
     const startD = moment(this.user.bookingdate).format('l');
-    console.log(startD);
     this.formValidate();
     if (this.user.bookingdate !== '') {
       this.isSelectedDate = true;
@@ -67,18 +68,19 @@ export class AddComponent implements OnInit {
   }
 
   formValidate() {
-    // const date1 = new Date(this.user.endtime);
-    // const date2 = new Date(this.user.starttime);
-    // const diff = (date1.getTime() - date2.getTime()) / 3600000;
-    // if (diff > 0 && diff <= 1) {
-    //   this.isValidTime = true;
-    // } else {
-    //   this.isValidTime = false;
-    // }
     if (this.user.name !== '' && this.user.contact !== '') {
       return false;
     } else {
       return true;
+    }
+  }
+
+  bookASlot() {
+    if (!this.formValidate()) {
+      const dateFormat = new Date(this.user.starttime);
+      const newDate = dateFormat.setMinutes(dateFormat.getMinutes() +  this.endDuration); // new date after adding 30 or 60 min
+      this.user.endtime = new Date(newDate);
+      console.log(this.user);
     }
   }
 
