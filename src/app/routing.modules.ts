e9736022c671fdbf1409fd14de41b4f235bc3f13
Your Name <DM00507444@techmahindra.com>
2019-08-28 11:56:33 +0530

    routing changed

--- src/app/routing.modules.ts
+++ src/app/routing.modules.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'details', component: DetailsComponent}
 ];
