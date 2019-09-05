46c1e17f519e4c616ef72a3ef7b18d5164b021b2
subhamchand <chand.subham95@gmail.com>
2019-09-05 07:19:35 +0000

    class timings from database

--- src/app/open-timing/open-timing.component.ts
+++ src/app/open-timing/open-timing.component.ts
@@ -11,7 +11,7 @@ export class OpenTimingComponent implements OnInit {
     constructor(private dateService: DateService) { }
     openTimeArray = [];
     ngOnInit() {
-        // this.getOpenTiming();
+        this.getOpenTiming();
     }
 
     getOpenTiming() {
@@ -22,7 +22,10 @@ export class OpenTimingComponent implements OnInit {
                     ...item.payload.doc.data()
                 };
             });
-            //   console.log(" this.openTimeArray", this.openTimeArray);
+            this.openTimeArray = this.openTimeArray.sort(function(a, b) {
+                return a.dayId - b.dayId  ||  a.name.localeCompare(b.name);
+            });
+              console.log(" this.openTimeArray", this.openTimeArray);
         });
     }
     getFirstHalfTime(object) {
@@ -31,18 +34,24 @@ export class OpenTimingComponent implements OnInit {
         if(object.mstartTime.hr == 0 && object.mendTime.hr == 0){
             return 'Closed';
         }
+        if(object.mstartTime.min == 0){
+            object.mstartTime.min = "00";
+        }
+        if(object.mendTime.min == 0){
+            object.mendTime.min = "00";
+        }
         if (object.mstartTime.hr > 12) {
-            sTime = (object.mstartTime.hr - 12) + ':' + object.mstartTime.min + ' pm';
-            eTime = (object.mendTime.hr - 12) + ':' + object.mendTime.min + ' pm';
+            sTime = (object.mstartTime.hr - 12) + ':' + object.mstartTime.min + ' PM';
+            eTime = (object.mendTime.hr - 12) + ':' + object.mendTime.min + ' PM';
         } else if (object.mstartTime.hr < 12 && object.mendTime.hr == 12) {
-            sTime = object.mstartTime.hr + ':' + object.mstartTime.min + ' am';
-            eTime = object.mendTime.hr + ':' + object.mendTime.min + ' pm';
+            sTime = object.mstartTime.hr + ':' + object.mstartTime.min + ' AM';
+            eTime = object.mendTime.hr + ':' + object.mendTime.min + ' PM';
         } else if (object.mstartTime.hr < 12 && object.mendTime.hr > 12) {
-            sTime = object.mstartTime.hr + ':' + object.mstartTime.min + ' am';
-            eTime = (object.mendTime.hr - 12) + ':' + object.mendTime.min + ' pm';
+            sTime = object.mstartTime.hr + ':' + object.mstartTime.min + ' AM';
+            eTime = (object.mendTime.hr - 12) + ':' + object.mendTime.min + ' PM';
         } else {
-            sTime = object.mstartTime.hr + ':' + object.mstartTime.min + ' am';
-            eTime = object.mendTime.hr + ':' + object.mendTime.min + ' am';
+            sTime = object.mstartTime.hr + ':' + object.mstartTime.min + ' AM';
+            eTime = object.mendTime.hr + ':' + object.mendTime.min + ' AM';
         }
         return sTime + '  to  ' + eTime;
     }
@@ -50,15 +59,21 @@ export class OpenTimingComponent implements OnInit {
     getSecondHalfTime(object) {
         let sTime;
         let eTime;
+        if(object.estartTime.min == 0){
+            object.estartTime.min = "00";
+        }
+        if(object.eendTime.min == 0){
+            object.eendTime.min = "00";
+        }
         if (object.estartTime.hr > 12) {
-            sTime = (object.estartTime.hr - 12) + ':' + object.estartTime.min + ' pm';
-            eTime = (object.eendTime.hr - 12) + ':' + object.eendTime.min + ' pm';
+            sTime = (object.estartTime.hr - 12) + ':' + object.estartTime.min + ' PM';
+            eTime = (object.eendTime.hr - 12) + ':' + object.eendTime.min + ' PM';
         } else if (object.estartTime.hr < 12 && object.eendTime.hr > 12) {
-            sTime = object.estartTime.hr + ':' + object.estartTime.min + ' am';
-            eTime = (object.eendTime.hr - 12) + ':' + object.eendTime.min + ' pm';
+            sTime = object.estartTime.hr + ':' + object.estartTime.min + ' AM';
+            eTime = (object.eendTime.hr - 12) + ':' + object.eendTime.min + ' PM';
         } else if(object.estartTime.hr == 12 && object.eendTime.hr > 12){
-            sTime = object.estartTime.hr + ':' + object.estartTime.min + ' pm';
-            eTime = (object.eendTime.hr - 12) + ':' + object.eendTime.min + ' pm';
+            sTime = object.estartTime.hr + ':' + object.estartTime.min + ' PM';
+            eTime = (object.eendTime.hr - 12) + ':' + object.eendTime.min + ' PM';
         }
         return sTime + '  to  ' + eTime;
     }
