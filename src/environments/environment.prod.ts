28cdc27bbee1496fac9601375dc9e24c5239843b
subhamchand <chand.subham95@gmail.com>
2019-09-11 06:06:05 +0000

    changed firebaseConfig in env.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC6FDsdjVHLVMW1FmtpbnNf7TdZqlytVfE',
+    authDomain: 'muziclubapp.firebaseapp.com',
+    databaseURL: 'https://muziclubapp.firebaseio.com',
+    projectId: 'muziclubapp',
+    storageBucket: '',
+    messagingSenderId: '203822538405',
+    appId: '1:203822538405:web:2eb3dde6700ed3a4'
+  }
 };
